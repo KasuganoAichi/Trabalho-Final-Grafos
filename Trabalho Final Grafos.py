fd57f4158b1cf2f4d07d8be27cb3bee1ee288920
KasuganoAichi <terriergamer@gmail.com>
2018-11-21 13:36:02 -0200

    Update Trabalho Final Grafos.py

--- Trabalho Final Grafos.py
+++ Trabalho Final Grafos.py
@@ -32,66 +32,50 @@ def converttoint(listint, x):
    NECESSÁRIO PARA CONECTAR TODOS OS
    CAMPUS DA UNIVERSIDADE E O RETORNA"""
 
-def dijsktra(grid, routers, listbad, nbad):
+def prim(grid, routers, listbad):
    """INDÍCES DA MATRIZ
       VALORES INICIAM EM 1 POIS
       QUANDO CHAMADOS PELA PRIMEIRA VEZ
       NECESSITAM ESTAR EM 1"""
-   i = 1 """COLUNA"""
-   imax = 0
+   i = 0"""LINHA"""
+   j = 0 """COLUNA"""
    
-   """MATRIZ COM OS VÉRTICES A SEREM UTILIZADOS"""
+   """MATRIZ SER UTILIZADA"""
    grid2 = list()
    
    """AUXILIARES"""
    auxlist = list(0)
+   usedlistbad = list()
+   total = 0
    menor = 0
    pos = 0
-   pred = 0
+   aux = 0
    
-   while routers >= 0:
-      grid2 = grid2.append(imax)
-      imax = imax + 1
-      routers = routers - 1
+   while len(auxlist) < routers:
+      menor = 999999
+      for item in auxlist:
+         i = auxlist[aux]
+         if i not in usedlistbad:   
+            while j <= routers:
+               grid2.append(grid[i][j])
+               j = j + 1
+            j = 0
+            for item in grid2:
+               if j not in listbad or i not in listbad:
+                  if grid2[j] < menor:
+                     if j not in auxlist and grid2[j] != 0:
+                        menor = grid2[j]
+                        pos = j
+                  j = j + 1
+            j = 0
+            aux = aux + 1
+      total = total + menor
+      aux = 0
+      auxlist.append(pos)
+      if pos in listbad:
+         usedlistbad.append(pos)
+   return total   
       
-   imax = imax - 1
-   
-   while i <= imax:
-      if (i + 1) in listbad:
-         auxlist = auxlist.append(0)
-      else:
-         auxlist = auxlist.append(grid[0][i])
-      i = i + 1
-   
-   grid2 = grid2.append(auxlist)
-   auxlist = list(0)
-   i = 0
-   
-   while i <= jmax:
-      if (i + 1) in listbad:
-         auxlist = auxlist.append(0)
-      else:
-         auxlist = auxlist.append('X')
-      i = i + 1
-
-   while 'X' in grid2:
-      i = 0
-      for item in grid2[0]
-         if menor > grid2[1][i]:
-            if grid2[2][i] != 'X':
-               menor = grid2[1][i]
-               pos = i
-         i = i + 1
-      grid2[2][pos] = pred
-      pred = pos
-      i = 0
-      for item in grid2[0]:
-         if (i + 1) not in listbad:
-            grid2[1][i] = grid2[1][i] + grid[pred][i]
-         i = i + 1
-   auxlist = grid2[1]
-   
-   return sum(auxlist)
    
 
 """Inicio do main"""
